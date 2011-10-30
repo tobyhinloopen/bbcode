dfb876711a04d35df1a92ef9460b7544080c4f10
Toby Hinloopen <toby@kutcomputers.nl>
2011-10-30 17:33:26 +0100

    Added tests for switching handlers

--- spec/handler_spec.rb
+++ spec/handler_spec.rb
@@ -1,14 +1,24 @@
 require 'spec_helper.rb'
 
 def get_handled_parser_result( string )
+	quote_handler = Bbcode::Handler.new()
+
 	handler = Bbcode::Handler.new({
 		:b => ->(element){ "<strong>#{element.content}</strong>" },
 		:i => ->(element){ "<em>#{element.content}</em>" },
 		:url => ->(element){ %(<a href="#{CGI.escapeHTML(element[0])}">#{element.content}</a>) },
 		:txt => ->(element){ "#{element.content.source}" },
+		:img => ->(element){ %(<img src="#{element.content.source}">) },
+		:quote => ->(element){ %(<blockquote>#{element.content.with_handler(quote_handler)}</blockquote>) },
 		:color => ->(element){ %(<span style="color: #{CGI.escapeHTML(element[0])};">#{element.content}</span>) },
 		:"#text" => ->(text){ CGI.escapeHTML(text) }
 	})
+
+	quote_handler.register_element_handlers handler.element_handlers.merge({
+		:img => ->(element){ %(<a href="#{element.content.source}">image</a>) },
+		:quote => ->(element){ "[...]" }
+	})
+
 	parser = Bbcode::Parser.new Bbcode::Tokenizer.new
 	parser.parse string, &handler.get_parser_handler
 	"#{handler.get_document.content}"
@@ -50,5 +60,10 @@ describe Bbcode::Handler do
 	it "should be able to render the source of an element's contents" do
 		get_handled_parser_result("[txt][b]ignored element[/b][/txt]").should \
 			eql("[b]ignored element[/b]")
+	end
+
+	it "should be able to swap handlers" do
+		get_handled_parser_result("[quote][img]epic.jpg[/img][/quote][img]epic.jpg[/img]").should \
+			eql(%(<blockquote><a href="epic.jpg">image</a></blockquote><img src="epic.jpg">))
 	end
 end
