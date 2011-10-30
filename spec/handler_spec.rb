ad9c43c5b16161c1a1316e7445edba66283bdecb
Toby Hinloopen <toby@kutcomputers.nl>
2011-10-30 15:42:13 +0100

    Added handler test for testing attributes and interrupts

--- spec/handler_spec.rb
+++ spec/handler_spec.rb
@@ -4,6 +4,7 @@ def get_handled_parser_result( string )
 	handler = Bbcode::Handler.new({
 		:b => ->(element){ "<strong>#{element.content}</strong>" },
 		:i => ->(element){ "<em>#{element.content}</em>" },
+		:url => ->(element){ %(<a href="#{CGI.escapeHTML(element[0])}">#{element.content}</a>) },
 		:"#text" => ->(text){ CGI.escapeHTML(text) }
 	})
 	parser = Bbcode::Parser.new Bbcode::Tokenizer.new
@@ -27,5 +28,15 @@ describe Bbcode::Handler do
 	it "should handle nested bbcode tags" do
 		get_handled_parser_result("[b]bold and [i]italic[/][/]").should \
 			eql("<strong>bold and <em>italic</em></strong>")
+	end
+
+	it "should handle attributes in a bbcode tag" do
+		get_handled_parser_result("[url=http://google.com/]google[/url]").should \
+			eql(%(<a href="http://google.com/">google</a>))
+	end
+
+	it "should handle basic tag interrupts" do
+		get_handled_parser_result("[b]bold[i]and italic[/b]only italic[/i]").should \
+			eql("<strong>bold<em>and italic</em></strong><em>only italic</em>")
 	end
 end
